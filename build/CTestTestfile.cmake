4ec11ea679cd3344c095023f68f7a735298f9b65
Amandine Fratani <fratani1u@etu.univ-lorraine.fr>
2021-10-05 16:21:52 +0200

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: O:/Documents/ENSG3A/Genie Logiciel/CMake/First_CMake_v3
-# Build directory: O:/Documents/ENSG3A/Genie Logiciel/CMake/First_CMake_v3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/log_lib")
-subdirs("mains")
-subdirs("tests")
